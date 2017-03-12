bb56142aa3e87c1b87c21c2938666a8105600f59
pooya-tolideh <pooya.tolideh@gmail.com>
2017-03-12 16:02:22 -0400

    added coordinate props to Maze

--- RoboMaze/js/maze.js
+++ RoboMaze/js/maze.js
@@ -3,5 +3,11 @@
 function Maze(width, height) {
     this.width = width;
     this.height = height;
+
+    this.startX             = null;
+    this.startY             = null;
+    this.startOrientation   = null;
+    this.endX               = null;
+    this.endY               = null;
 }
 
