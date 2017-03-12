4b6e2ba69e2c25b544025b03fa18aabd4466b026
pooya-tolideh <pooya.tolideh@gmail.com>
2017-03-12 18:02:41 -0400

    check direction in setWall and setStart using isValidDir

--- RoboMaze/js/maze.js
+++ RoboMaze/js/maze.js
@@ -20,9 +20,11 @@ function Maze(width, height) {
 }
 
 Maze.prototype.setStart = function (x, y, orientation) {
-    this.startX = x;
-    this.startY = y;
-    this.startOrientation = orientation;
+    if (this.isValidDir(orientation) {
+        this.startX = x;
+        this.startY = y;
+        this.startOrientation = orientation;
+    }
 }
 
 Maze.prototype.setEnd = function (x, y) {
@@ -38,9 +40,7 @@ Maze.prototype.setEnd = function (x, y) {
 
 Maze.prototype.setWall = function(x,y,direction) {
     validCo = x > 0 && x <= this.width && y > 0 && y <= this.height;
-    validDir = this.directions.indexOf(direction) !== -1;
-
-    if ( validCo && validDir ) {
+    if ( validCo && this.isValidDir(direction)) {
         this.spaces[x][y].setWall(direction);
         return true;
     }
