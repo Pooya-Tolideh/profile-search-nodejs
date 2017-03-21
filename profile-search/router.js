b8c8e0ecbdba3590570407078854ac5327e11435
pooya-tolideh <pooya.tolideh@gmail.com>
2017-03-21 16:10:36 -0400

    added route functions as modules to router.js

---
+++ profile-search/router.js
@@ -0,0 +1,23 @@
+const home = function (req, res) {
+    if (req.url === "/") {
+        res.writeHead(200, {'Content-Type' : 'text/plain'});
+        res.write('Header\n');
+        res.write('Search Here...\n');
+        res.end('Footer');
+    }
+};
+
+const user = function (req, res) {
+    let username = req.url.replace('', '/');
+    if (username.length > 0) {
+            res.writeHead(200, {'Content-Type' : 'text/plain'});
+            res.write('Header\n');
+            res.write('username\n');
+            res.end('Footer');
+    }
+};
+
+module.export = {
+    userRoute : home,
+    homeRoute : user
+}
