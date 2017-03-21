4fa10eeb5dbf79c1592bd3c94392fd447c61d88c
pooya-tolideh <pooya.tolideh@gmail.com>
2017-03-21 16:12:03 -0400

    added router.js as a module to server app

--- profile-search/app.js
+++ profile-search/app.js
@@ -1,28 +1,10 @@
 console.log('./START');
 
 const http = require('http');
+const router = require('./router.js');
 
 http.createServer((req, res) => {
-    homeRoute(req, res);
+    router.home(req, res);
 }).listen(3000);
 
-function homeRoute(req, res) {
-    if (req.url === "/") {
-        res.writeHead(200, {'Content-Type' : 'text/plain'});
-        res.write('Header\n');
-        res.write('Search Here...\n');
-        res.end('Footer');
-    }
-}
-
-function userRoute(req, res) {
-    let username = req.url.replace('', '/');
-    if (username.length > 0) {
-            res.writeHead(200, {'Content-Type' : 'text/plain'});
-            res.write('Header\n');
-            res.write('username\n');
-            res.end('Footer');
-    }
-
-}
 console.log('./END');
