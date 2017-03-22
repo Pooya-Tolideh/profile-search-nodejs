37e0a896c90844f14ad21c41acc9a126e23e654b
pooya-tolideh <pooya.tolideh@gmail.com>
2017-03-22 16:20:39 -0400

    created Profile Constructor
    this constructor takes the username and adds it to the api link. It
    also has a getInfo method that gets the api data

--- profile-search/profile-api.js
+++ profile-search/profile-api.js
@@ -1,4 +1,26 @@
 const http = require('http');
 const https = require('https');
-const EventEmitter = require('events').EventEmitter;
+const events = require('events');
 const util = require('util');
+
+
+function Profile(username) {
+    this.url = `https://teamtreehouse.com/${username}.json`
+}
+
+util.inherits(Profile, events.EventEmitter);
+
+Profile.prototype.getInfo = function () {
+    https.get(this.url, req => {
+        let body = '';
+        req.on('data', packet => {
+            body += packet.toString();
+        });
+        req.on('end', () => {
+            const profile = JSON.parse(body);
+            this.emit('api-received', profile);
+        });
+    });
+}
+
+
