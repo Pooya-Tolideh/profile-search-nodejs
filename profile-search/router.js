26bc0ea9844fdcd28be953d4a1ab380508b792be
pooya-tolideh <pooya.tolideh@gmail.com>
2017-03-22 22:20:06 -0400

    print user badges and name as a test

--- profile-search/router.js
+++ profile-search/router.js
@@ -8,22 +8,24 @@ const homeRoute = function (req, res) {
         res.write('Header\n');
         res.write('Search Here...\n');
         res.end('Footer');
+        console.log('./HOME END');
     }
 };
 
 const userRoute = function (req, res) {
-    let username = req.url.replace('/', '');
-    if (username.length > 0) {
-            res.writeHead(200, {'Content-Type' : 'text/plain'});
-            res.write('Header\n');
-            const studentProfile = new Profile('pooyatolideh');
-            studentProfile.on('api-received', profile => {
-                const studentInfo = extractInfo(profile);
-                res.write(JSON.stringify(studentInfo));
-                res.end('Footer');
-            });
-            studentProfile.getInfo();
-    }
+        let username = req.url.replace('/', '');
+        if (username.length > 0) {
+                res.writeHead(200, {'Content-Type' : 'text/plain'});
+                res.write('Header\n');
+                const studentProfile = new Profile(username);
+                studentProfile.on('api-received', profile => {
+                    const stuInfo = extractInfo(profile);
+                    res.write(`${stuInfo.username} has ${stuInfo.badges} badges`);
+                    res.end('Footer');
+                    console.log('./USER END');
+                });
+                studentProfile.getInfo();
+            }
 };
 
 module.exports = {
