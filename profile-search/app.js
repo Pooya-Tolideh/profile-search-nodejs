41347c00776a905e0e9cde7c847629bee0ff8466
pooya-tolideh <pooya.tolideh@gmail.com>
2017-03-23 15:25:45 -0400

    Adds serveCSS to app.js
    adding it to the top ensures it loads before anything else

--- profile-search/app.js
+++ profile-search/app.js
@@ -5,6 +5,7 @@ const router = require('./router.js');
 
 http.createServer((req, res) => {
     try {
+        router.serveCSS(req, res);
         router.home(req, res);
         router.user(req, res);
     } catch (error) {console.log(error.message)}
