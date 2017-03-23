dea3fce52db6a2ca80ca1a765d5ab3a751426941
pooya-tolideh <pooya.tolideh@gmail.com>
2017-03-23 02:10:18 -0400

    added merge to render
    merge is a template engine. scans through the html template and
    replaces moustache placeholders with given key-value pairs

--- profile-search/render.js
+++ profile-search/render.js
@@ -1,9 +1,19 @@
 const fs = require('fs');
-function render (fileName, res) {
+
+function merge(template, values) {
+    for (key in values) {
+        console.log(key, values[key]);
+        template = template.replace(`{{${key}}}` , values[key]);
+    }
+    return template;
+}
+
+function render (fileName, values ,res) {
     //get the html templates
-    const templateFile = fs.readFileSync(__dirname + '/views/' + `${fileName}.html`, 'utf8');
+    let templateFile = fs.readFileSync(__dirname + '/views/' + `${fileName}.html`, 'utf8');
 
     //replace in the values
+    templateFile = merge(templateFile, values);
 
     //send the response
     res.write(templateFile);
